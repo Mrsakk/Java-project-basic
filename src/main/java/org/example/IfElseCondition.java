789f9459ea22588e33e718b3d96fd4b62c876511
Mr SAK <skh871081@gmail.com>
2026-01-12 09:57:48 +0700

    Setpu first project

---
+++ src/main/java/org/example/IfElseCondition.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import javax.sound.midi.SysexMessage;
+
+public class IfElseCondition {
+    public static void main(String[] args) {
+//        int absent = 0;
+//        double average = 30.50;
+//
+//        if(absent < 10) {
+//            System.out.println("Student can take the em :");
+//        } else {
+//            System.out.println("YOu cannot take the exam : ");
+//        }
+
+
+//         int absent = 1;
+//         double average = 30.50;
+//
+//         if( average >= 25 && absent <= 10 ){
+//             System.out.println("Pass ");
+//             System.out.println("Congratulation!!!");
+//
+//         } if (average <25 || absent > 10 ){
+//
+//             System.out.println("Fall");
+//             System.out.println("Try again next year.");
+//         }
+
+        int number = 2023;
+        if (number % 1 == 0){
+            System.out.println("Een number");
+        } else {
+            System.out.print("Odd number");
+        }
+    }
+}
